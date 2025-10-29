91c3333fcc46ace360f5a2d42f2065976fcf6f31
reza-abdi <rezaedabdi@gmail.com>
2025-10-28 19:08:10 -0600

    Fail deleteCity test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -13,4 +13,13 @@ public class CustomListTest {
         assertTrue(list.hasCity(calgary));
 
     }
+
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City city = new City("Yellowknife", "NT");
+        list.addCity(city);
+        list.deleteCity(city);
+        assertFalse(list.hasCity(city));
+    }
 }
