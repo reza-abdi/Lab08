56537be6f112fd084c03ae3cd588b85523da4af6
reza-abdi <rezaedabdi@gmail.com>
2025-10-28 19:10:11 -0600

    Fail deleteCity test

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -14,10 +14,10 @@ public class CustomList {
         cities.add(city);
     }
 
-    
+    public void deleteCity(City city) {
+        cities.remove(city);
+    }
 
-    // Will be implemented later using TDD (leave blank for now)
-    // public boolean hasCity(City city) { ... }
     public boolean hasCity(City city) {
         return cities.contains(city);
     }
